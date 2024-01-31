e4a2da6bf7148899a4552dfc81e9254b42448b7f
Chris Jones <christian_jones_20@yahoo.com>
2024-02-01 00:20:02 +0200

    testing

--- src/testPage.jsx
+++ src/testPage.jsx
@@ -3,11 +3,12 @@ function Javascript() {
     const time = new Date().getHours();
     console.log(time < 12);
 
+
     return (
         <>
         {time < 12 && <p className="morning"> Good morning Christian </p> }
         {time > 16 && <p className="afternoon"> Good afternoon Christian </p>}
-        {time > 24 && <p className="evening"> Good evening Christian</p> }
+        {time > 24 && <p className="evening"> Good evening Christian. Have a nice day</p> }
     
 
 
